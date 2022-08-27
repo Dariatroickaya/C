da3c805d911a49653dca1a05dac00f57c4f28b94
Daria <firefly88@mail.ru>
2022-08-28 01:50:10 +0600

    add example010_find

---
+++ example010_find/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {12,12,31,4,51,6,17,18};
+
+int n = array.Length;
+int find = 12;
+
+int index = 0;
+
+while (index < n)
+{
+if(array[index] == find)
+{
+    Console.WriteLine(index);
+}
+    index++;
+}
